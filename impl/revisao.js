d138bb55cf511742b7e4426c73b4b96f821dc7cb
EsterMarcos <estermarcoshas@gmail.com>
2019-08-14 20:16:09 -0300

    revisao.js

---
+++ impl/revisao.js
@@ -0,0 +1,13 @@
+{
+    var nota1 = 10;
+    var nota2 = 9;
+    var nota3 = 5;
+    var med = (nota1 + nota2 + nota3) / 3;
+    console.log("A media é " + med);
+    if (med >= 7) {
+        console.log("aprovado");
+    }
+    else {
+        console.log("reprovado");
+    }
+}
---
+++ revisao.ts
@@ -0,0 +1,18 @@
+{
+    let nota1 : number = 10;
+    let nota2 : number = 9;
+    let nota3 : number = 5;
+    
+    let med : number = (nota1 + nota2 + nota3)/3;
+
+    console.log ("A media é "+ med);
+
+    if(med>=7)
+    {
+        console.log ( "aprovado");
+    }
+    else
+    {
+        console.log ( "reprovado");
+    }
+}
