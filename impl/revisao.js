5aaf3385e06d2f6be1cb3ef4f28b9d288b2e36e6
EsterMarcos <estermarcoshas@gmail.com>
2019-08-14 22:15:19 -0300

    Valor padrão para função

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,9 +1,13 @@
 {
     var n1 = 10;
     var n2 = 9;
-    var n3 = 5;
-    var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
+    //let n3 : number = 5;
+    var fMedia = function (nota1, nota2, nota3) {
+        if (nota3 === void 0) { nota3 = 7; }
+        return (nota1 + nota2 + nota3) / 3;
+    };
     // acima a forma simplificada de uma função
-    console.log("A media \u00E9 " + fMedia(n1, n2, n3));
+    //no caso da ausenncia de um dado pode se colocar sua equivalencia diretamente na função
+    console.log("A media \u00E9 " + fMedia(n1, n2));
     // com o uso do cifrão e da craze exibe-se o texto de uma forma mais simples
 }
--- revisao.ts
+++ revisao.ts
@@ -1,12 +1,12 @@
 {
     let n1 : number = 10;
     let n2 : number = 9;
-    let n3 : number = 5;
+    //let n3 : number = 5;
 
-    let fMedia = (nota1:number,nota2:number,nota3:number)=>(nota1+nota2+nota3) /3  
+    let fMedia = (nota1:number,nota2:number,nota3:number=7)=>(nota1+nota2+nota3) /3  
     // acima a forma simplificada de uma função
-
-    console.log (`A media é ${ fMedia(n1,n2,n3)}`);
+    //no caso da ausenncia de um dado pode se colocar sua equivalencia diretamente na função
+    console.log (`A media é ${ fMedia(n1,n2)}`);
     // com o uso do cifrão e da craze exibe-se o texto de uma forma mais simples
 
     
