1d24ae3f18fb85f529933fbf63cde759823024bd
EsterMarcos <estermarcoshas@gmail.com>
2019-08-14 22:29:03 -0300

    Exercico de calcular volume com função

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+{
+    let comp : number = 10;
+    let alt  : number = 5;
+    let larg : number = 5;
+
+
+    let volume = (comprimento:number,altura:number,largura:number)=>(comprimento*altura*largura) 
+    console.log (`A media é ${ volume(comp,alt,larg)}`)
+    
+}
---
+++ impl/exercicio.js
@@ -0,0 +1,7 @@
+{
+    var comp = 10;
+    var alt = 5;
+    var larg = 5;
+    var volume = function (comprimento, altura, largura) { return (comprimento * altura * largura); };
+    console.log("A media \u00E9 " + volume(comp, alt, larg));
+}
