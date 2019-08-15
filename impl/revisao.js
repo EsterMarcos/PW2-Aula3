1a67b81a8bd56c196b9292c2f30d52787536a717
EsterMarcos <estermarcoshas@gmail.com>
2019-08-14 21:58:01 -0300

    Utilização da função seta

--- impl/revisao.js
+++ impl/revisao.js
@@ -2,9 +2,8 @@
     var n1 = 10;
     var n2 = 9;
     var n3 = 5;
-    //let med : number = (nota1 + nota2 + nota3)/3;
-    var fMedia = function (nota1, nota2, nota3) {
-        return (nota1 + nota2 + nota3) / 3;
-    };
-    console.log("A media \u00E9 " + fMedia(n1, n2, n3)); // com o uso do cifrão e da craze exibe-se o texto de uma forma mais simples
+    var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
+    // acima a forma simplificada de uma função
+    console.log("A media \u00E9 " + fMedia(n1, n2, n3));
+    // com o uso do cifrão e da craze exibe-se o texto de uma forma mais simples
 }
